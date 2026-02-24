fe19a6702e79c6449b5951fddff82166ad7d9041
Mohammad Mahdi Shokor <mohammadmahdishokor@gmail.com>
2026-02-24 13:45:44 +0200

    triva

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"587c18f873b8ab57330422bce09047420d9c7f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1249570861"
+    serviceWorkerVersion: "2670107775"
   }
 });
